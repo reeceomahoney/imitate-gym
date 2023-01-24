acc8e31d32588fa6e460ec07a857dcb9cecf17e2
reece <reece.omahoney3@gmail.com>
2023-01-24 20:02:37 +0000

    add AMP components

--- scripts/amp/runner.py
+++ scripts/amp/runner.py
@@ -7,9 +7,11 @@ import argparse
 import numpy as np
 
 import torch
-import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
 
 import random
+from datetime import datetime
+from copy import deepcopy
 
 from ruamel.yaml import YAML, dump, RoundTripDumper
 
@@ -21,7 +23,8 @@ import modules
 from raisim_gym_torch.env.RaisimGymVecEnv import RaisimGymVecEnv as VecEnv
 from raisim_gym_torch.helper.raisim_gym_helper import ConfigurationSaver, load_param, tensorboard_launcher, RewardLogger
 
-import raisim_gym_torch.algo.ppo.ppo as PPO
+from raisim_gym_torch.algo.ppo.ppo import PPO
+from raisim_gym_torch.algo.amp.amp import AMP
 
 
 def main():
@@ -44,13 +47,21 @@ def main():
     task_path = home_path + "/gym_envs/" + task_name
 
     # config
-    cfg = YAML().load(open(task_path + "/cfg.yaml", 'r'))
+    cfg = YAML().load(open(task_path + "/cfg_amp.yaml", 'r'))
 
     if cfg['environment']['num_threads'] == 'auto':
         cfg['environment']['num_threads'] = psutil.cpu_count(logical=False)
 
     # create environment from the configuration file
     env = VecEnv(RaisimGymEnv(home_path + "/resources", dump(cfg['environment'], Dumper=RoundTripDumper)))
+
+    # load expert dataset
+    disc_half_ob_dim = 45
+    expert_dataset = np.loadtxt(home_path + "/resources/expert_data/expert_data_processed_w_feet.csv", delimiter=",")
+    expert_dataset = expert_dataset[:, :disc_half_ob_dim]  # remove initialisation data
+
+    # load style coeff from config
+    style_coeff = cfg['environment']['reward']['style']['coeff']
 
     # Set seed
     random.seed(cfg['seed'])
@@ -70,9 +81,15 @@ def main():
     _actor_critic_module = modules.get_actor_critic_module_from_config(cfg, env, NormalSampler, device)
     actor_critic_module_eval = modules.get_actor_critic_module_from_config(cfg, env, NormalSampler, device)
 
+    _discriminator_module = modules.get_discriminator_from_config(cfg, env, device)
+
     saver = ConfigurationSaver(log_dir=home_path + "/data/" + task_name,
-                               save_items=[task_path + "/cfg.yaml", task_path + "/Environment.hpp"])
+                               save_items=[task_path + "/cfg_amp.yaml", task_path + "/Environment.hpp"])
+
+    # rick server ip: 129.67.95.58
     tensorboard_launcher(saver.data_dir)  # press refresh (F5) after the first ppo update
+    log_dir = os.path.join(saver.data_dir, datetime.now().strftime('%b%d_%H-%M-%S'))
+    writer = SummaryWriter(log_dir=log_dir, flush_secs=10)
 
     # Discount factor
     rl_gamma = np.exp(cfg['environment']['control_dt'] * np.log(0.5) / cfg['algorithm']['gamma_half_life_duration'])
@@ -83,7 +100,7 @@ def main():
     if cfg['algorithm']['learning_rate']['mode'] == 'constant':
         learning_rate_decay_gamma = 1.
 
-    ppo = PPO.PPO(
+    ppo = PPO(
         actor_critic_module=_actor_critic_module,
         num_envs=cfg['environment']['num_envs'],
         num_transitions_per_env=n_steps,
@@ -91,6 +108,7 @@ def main():
         gamma=rl_gamma,
         lam=0.95,
         num_mini_batches=4,
+        writer=writer,
         device=device,
         log_dir=saver.data_dir,
         learning_rate=cfg['algorithm']['learning_rate']['initial'],
@@ -98,6 +116,18 @@ def main():
         learning_rate_schedule=cfg['algorithm']['learning_rate']['mode'],
         learning_rate_min=cfg['algorithm']['learning_rate']['min'],
         decay_gamma=learning_rate_decay_gamma
+    )
+
+    amp = AMP(
+        discriminator=_discriminator_module,
+        ppo=ppo,
+        num_envs=cfg['environment']['num_envs'],
+        num_transitions_per_env=n_steps,
+        num_learning_epochs=1,
+        mini_batch_size=256,
+        expert_dataset=expert_dataset,
+        writer=writer,
+        device=device
     )
 
     if mode == 'retrain':
@@ -116,6 +146,10 @@ def main():
 
         reward_ll_sum = 0
         done_sum = 0
+
+        obs_mean = np.zeros(env.num_obs, dtype=np.float32)
+        obs_var = np.zeros(env.num_obs, dtype=np.float32)
+        obs_count = 0.0
 
         visualizable_iteration = False
 
@@ -165,14 +199,24 @@ def main():
         # actual training
         for step in range(n_steps):
             obs = env.observe()
-            action = ppo.act(obs)
+            obs_1 = deepcopy(obs)  # make a deepcopy to prevent obs_1 and obs_2 pointing to the same object
+
+            action = ppo.act(obs_1)
             reward, dones = env.step(action)
+
+            # collect second observation
+            obs_2 = env.observe(False)
+
+            # compute style rewards
+            style_reward = style_coeff * amp.step(obs_1, obs_2)
+            reward += style_reward
+
             ppo.step(rews=reward, dones=dones)
             done_sum = done_sum + np.sum(dones)
             reward_ll_sum = reward_ll_sum + np.sum(reward)
 
             # Store the rewards for this step
-            reward_logger.step()
+            reward_logger.step(style_reward)
 
         # take st step to get value obs
         obs = env.observe()
@@ -186,6 +230,9 @@ def main():
             if log_this_iteration:
                 last_ppo_log_iter = update
 
+            env.wrapper.getObStatistics(obs_mean, obs_var, obs_count)
+            amp.update(log_this_iteration=update % 10 == 0, update=update,
+                       obs_mean=obs_mean[:disc_half_ob_dim], obs_var=obs_var[:disc_half_ob_dim])
             ppo.update(obs=obs, log_this_iteration=log_this_iteration, update=update)
 
             ppo.actor_critic_module.update()
